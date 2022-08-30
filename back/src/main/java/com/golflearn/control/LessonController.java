a23d3892e0bcff954291c7566b039090a87cf165
cokesh <spdlqjlwjs@daum.net>
2022-08-30 17:58:58 +0900

    [수정] LessonController - viewMain, lessonHistory

--- back/src/main/java/com/golflearn/control/LessonController.java
+++ back/src/main/java/com/golflearn/control/LessonController.java
@@ -3,54 +3,67 @@ package com.golflearn.control;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.golflearn.dto.Lesson;
 import com.golflearn.dto.LessonLine;
+import com.golflearn.dto.ResultBean;
 import com.golflearn.exception.FindException;
 import com.golflearn.service.LessonService;
 
-@Controller
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("lesson/*")
 public class LessonController {
 	@Autowired
 	private LessonService service;
 	
-	@GetMapping("viewmain")
-	public Object viewMain() { //로그인 유무와 상관없이 다 볼수 있기때문에 httpSession 필요없음
-		Map<String,Object> map = new HashMap<>();
+	@GetMapping(value= {""})
+	public ResultBean<Lesson> list(@PathVariable Optional<Integer> optCp) { //로그인 유무와 상관없이 다 볼수 있기때문에 httpSession 필요없음
+		ResultBean<Lesson> rb = new ResultBean<>();
 		try {
-			List<Lesson> lsns = service.viewMain();
-			return lsns;
-		} catch (FindException e) {				
+			List<Lesson> lessons = service.viewMain();
+			rb.setStatus(1);
+			rb.setLt(lessons);
+			return rb;
+		} catch (FindException e) {
 			e.printStackTrace();
-			map.put("status", -1);
-			map.put("msg", e.getMessage());
-			return map;
+			rb.setStatus(-1);
+			rb.setMsg(e.getMessage());
+			return rb;
 		}
 	}
-	
-	@GetMapping("viewhistory")
-	public Object viewHistory(HttpSession session, int lsnNo) {
-		Map<String,Object> map = new HashMap<>();
+
+	@GetMapping(value = {"lesson", "lesson/{optCp}"}) // 프로의 레슨내역에서 레슨번호에 대한 히스토리
+	public ResultBean<LessonLine> viewHistory(@PathVariable int optCp, HttpSession session) {
+		ResultBean<LessonLine> rb = new ResultBean<>();
+		// 로그인 여부를 받아와야한다 HttpSession?
 		String loginedId = (String)session.getAttribute("loginInfo");
 		if(loginedId == null) {
-			map.put("status", 0);
-			map.put("msg", "로그인하세요");
-			return map;
+			rb.setStatus(0);
+			rb.setMsg("로그인하세요");
+			return rb;
 		}else {
 			try {
-				List<LessonLine> lsnHistories = service.viewLessonHistory(lsnNo);
-				return lsnHistories;
+				List<LessonLine> lsnHistories = service.viewLessonHistory(optCp);
+				rb.setStatus(1);
+				rb.setLt(lsnHistories);
+				return rb;
 			} catch (FindException e) {				
 				e.printStackTrace();
-				map.put("status", -1);
-				map.put("msg", e.getMessage());
-				return map;
+				rb.setStatus(-1);
+				rb.setMsg(e.getMessage());
+				return rb;
 			}
 		}
 	}
