b2d6bce503ae7a57806b6eaf602aa1c517fbbc92
cokesh <spdlqjlwjs@daum.net>
2022-08-31 17:23:08 +0900

    [추가] noticeBoard - entity생성

--- BackNotice/src/main/java/com/golflearn/domain/NoticeComment.java
+++ BackNotice/src/main/java/com/golflearn/domain/NoticeComment.java
@@ -3,8 +3,17 @@ package com.golflearn.domain;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -14,13 +23,41 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
-@EqualsAndHashCode(of = {""})
+@EqualsAndHashCode(of = {"noticeCmtNo"})
+@Table(name= "notice_comment")
+@SequenceGenerator(name = "noticecomment_seq_generator",
+					sequenceName= "notice_comment_seq",
+					initialValue = 1,
+					allocationSize = 1
+					)
+@DynamicInsert
+@DynamicUpdate
 public class NoticeComment {
 	@Id
 	@GeneratedValue 
-	@Column
+	@ColumnDefault(value = "0")
 	private Long noticeCmtNo;
+	
+	@ManyToOne
+	@JoinColumn(name="notice_board_no")
+	private NoticeBoard noticeBoard;
+	
+	@Column(name="user_nickname")
+	private String userNickname;
+	
+	@Column(name="user_cmt_content")
+	private String noticeCmtContent;
+	
+	@Column(name="notice_dt")
+	@JsonFormat(pattern = "yy/MM/dd", timezone = "Asia/Seoul")
+	@ColumnDefault(value = "SYSDATE")
+	private Date noticeCmtDt;
+	
+	@Column(name="notice_cmt_parent_no")
+	@ColumnDefault(value = "-1")
+	private Long noticeCmtParentNo;
 }
--- BackNotice/src/main/java/com/golflearn/domain/NoticeLike.java
+++ BackNotice/src/main/java/com/golflearn/domain/NoticeLike.java
@@ -1,5 +1,49 @@
 package com.golflearn.domain;
 
-public class NoticeLike {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(of = {"noticeLikeNo"})
+@Table(name= "notice_like")
+@SequenceGenerator(name = "noticelike_seq_generator",
+					sequenceName= "notice_like_seq",
+					initialValue = 1,
+					allocationSize = 1
+					)
+@DynamicInsert
+@DynamicUpdate
+public class NoticeLike {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+					generator = "noticelike_seq_generator")
+	@Column(name="notice_like_no")
+	private Long noticeLikeNo;
+	
+	@ManyToOne
+	@JoinColumn(name="notice_board_no")
+	private NoticeBoard noticeBoard;
+	
+	@Column(name="user_nickname")
+	private String userNickname;
+	
 }
