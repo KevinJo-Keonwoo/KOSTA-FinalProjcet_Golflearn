deca608c28c989bac9dba7c47db8104e2464de58
future <rmh7290@gmail.com>
2022-08-31 12:27:22 +0900

    [추가]엔티티 설정 어노테이션

--- BackResale/src/main/java/com/golflearn/domain/ResaleComment.java
+++ BackResale/src/main/java/com/golflearn/domain/ResaleComment.java
@@ -2,14 +2,36 @@ package com.golflearn.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+@Entity
+@Table(name = "resale_comment")
 public class ResaleComment {
-	
+	@Id
+	@Column(name="resale_cmt_no")
 	private Long resaleCmtNo;
 //	Long resaleBoardNo;
+	
 	private ResaleBoard resaleBoard;
+	
+	@Column(name="resale_cmt_content")
 	private String resaleCmtContent;
+	
+	@JsonFormat(pattern = "yy/MM/dd", timezone = "Asia/Seoul")
+	@Column(name="resale_cmt_dt")
+	@ColumnDefault(value="SYSDATE")
 	private Date resaleCmtDt;
+	
+	@Column(name="resale_cmt_parent_no")
+	@ColumnDefault(value="0")
 	private Long resaleCmtParentNo;
-	private String userNickname;
+	
+//	private String userNickname;
 	
 }
