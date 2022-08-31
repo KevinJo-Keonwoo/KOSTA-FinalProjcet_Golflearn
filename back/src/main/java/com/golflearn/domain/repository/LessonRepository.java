0f3d500575c2ce31046fece47fe8e240634c86aa
Jiyeon <jiyeon.backend@gmail.com>
2022-08-31 15:17:58 +0900

    [삭제] insertLsnClassification 삭제

--- back/src/main/java/com/golflearn/domain/repository/LessonRepository.java
+++ back/src/main/java/com/golflearn/domain/repository/LessonRepository.java
@@ -1,9 +1,6 @@
 package com.golflearn.domain.repository;
 
-import java.util.List;
-
 import com.golflearn.dto.Lesson;
-import com.golflearn.dto.LessonClassification;
 import com.golflearn.exception.AddException;
 import com.golflearn.exception.FindException;
 
@@ -17,17 +14,10 @@ public interface LessonRepository {
 	public Lesson selectByLsnNo(int lsnNo) throws FindException;
 	
 	/**
-	 * 프로가 레슨을 승인요청한다 : 레슨의 레슨분류정보 외 정보
+	 * 프로가 레슨을 승인요청한다
 	 * @param Lesson 레슨정보
 	 * @throws AddException
 	 */
 	void insertLsnInfo(Lesson lesson) throws AddException;
-
-	/**
-	 * 프로가 레슨을 승인요청한다 : 레슨의 레슨분류정보 정보
-	 * @param lsnClassifications 레슨분류정보들
-	 * @throws AddException
-	 */
-	void insertLsnClassification(Lesson lesson) throws AddException;
 	
 }
