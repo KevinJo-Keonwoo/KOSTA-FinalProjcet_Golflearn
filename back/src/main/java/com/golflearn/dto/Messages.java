bd2485001e3645a1f5dcc941efea927c0830109b
yeon <choyeon960325@gmail.com>
2022-08-31 09:45:33 +0900

    [추가]sms API관련 dto&service 추가

---
+++ back/src/main/java/com/golflearn/dto/Messages.java
@@ -0,0 +1,15 @@
+package com.golflearn.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Messages {
+    private String to;
+    private String content;
+}
