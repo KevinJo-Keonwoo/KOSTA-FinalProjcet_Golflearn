24b6d99d200ca678f44218c0b0d88a208f5e65f7
yeon <choyeon960325@gmail.com>
2022-08-31 09:47:42 +0900

    [수정]설정 파일 코드 변경

--- back/src/main/java/com/golflearn/config/DatabaseConfiguration.java
+++ back/src/main/java/com/golflearn/config/DatabaseConfiguration.java
@@ -1,16 +1,17 @@
 package com.golflearn.config;
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-
 @Configuration
 //application.yml파일을 application.properties파일로 변경
 @PropertySource("classpath:/application.properties")
@@ -25,6 +26,13 @@ public class DatabaseConfiguration {
 		DataSource dataSource = new HikariDataSource(hikariConfig());
 		return dataSource;
 	}
-
-
+	@Autowired
+	private ApplicationContext applicationContext;
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {//<bean id="sqlSessionFactory">
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		sqlSessionFactoryBean.setConfigLocation(applicationContext.getResource("classpath:/mybatisConf/mybatis-config.xml"));
+		return sqlSessionFactoryBean.getObject();
+	}
 }
