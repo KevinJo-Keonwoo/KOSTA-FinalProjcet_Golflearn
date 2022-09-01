4a2143fdb93f4226134319b6eabeea1ad44f0ed2
future <rmh7290@gmail.com>
2022-09-02 01:24:03 +0900

    [수정] 주석 내용

--- BackResale/src/main/java/com/golflearn/domain/repository/ResaleLikeRepository.java
+++ BackResale/src/main/java/com/golflearn/domain/repository/ResaleLikeRepository.java
@@ -1,9 +1,9 @@
 package com.golflearn.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.golflearn.domain.entity.ResaleLikeEntity;
 
-public interface ResaleLikeRepository extends JpaRepository<ResaleLikeEntity, Long> {
+public interface ResaleLikeRepository extends CrudRepository<ResaleLikeEntity, Long> {
 
 }
