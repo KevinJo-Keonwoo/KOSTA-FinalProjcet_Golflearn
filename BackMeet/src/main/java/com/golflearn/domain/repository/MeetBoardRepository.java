690d9c1f3e17d10d2dc80d0f2b72894304708625
Jiyeon <jiyeon.backend@gmail.com>
2022-09-05 10:00:57 +0900

    [추가] 참여중인모임목록 보기 쿼리 추가

--- BackMeet/src/main/java/com/golflearn/domain/repository/MeetBoardRepository.java
+++ BackMeet/src/main/java/com/golflearn/domain/repository/MeetBoardRepository.java
@@ -21,7 +21,6 @@ public interface MeetBoardRepository extends JpaRepository<MeetBoardEntity, Long
 	List<MeetBoardEntity> findByPage(int startRow, int endRow);
 	//모든 목록을 페이징처리하여 최신순으로 보여준다
 	
-	
 	@Query(value ="SELECT *\r\n"
 			+ "FROM (SELECT rownum r, a.*\r\n"
 			+ "		  FROM (SELECT * \r\n"
@@ -55,11 +54,30 @@ public interface MeetBoardRepository extends JpaRepository<MeetBoardEntity, Long
 	List<MeetBoardEntity> findByWordAndPage(String word, int startRow, int endRow);
 	//검색어가 제목에 포함된 모임글의 목록을 불러온다
 	
-	
 	@Query(value ="	SELECT COUNT(*)\r\n"
 			+ "	FROM meet_board\r\n"
 			+ "	WHERE meet_board_title LIKE %?1%"
 			, nativeQuery = true)
 	int countByWord(String word);
 	//검색어가 제목에 포함된 모임글의 수를 반환한다
+	
+	@Query(value="SELECT *\r\n"
+			+ "        FROM (SELECT rownum r, a.*\r\n"
+			+ "		FROM (SELECT * \r\n"
+			+ "		FROM meet_board mb JOIN meet_member mm ON (mb.meet_board_no = mm.meet_board_no)\r\n"
+			+ "		WHERE mm.user_nickname = ?1\r\n"
+			+ "		ORDER BY mb.meet_board_no DESC\r\n"
+			+ "		) a\r\n"
+			+ "		)\r\n"
+			+ "		WHERE r BETWEEN ?2 AND ?3"
+			,nativeQuery = true)
+	List<MeetBoardEntity> findByUserNickNameAndPage(String userNickname, int startRow, int endRow);
+	//참여중인 모임글의 목록을 불러온다
+	
+	@Query(value ="	SELECT COUNT(*)\r\n"
+			+ "	FROM meet_board\r\n"
+			+ "	WHERE user_nickname = ?1"
+			, nativeQuery = true)
+	int countByUserNicakname(String userNickName);
+	//참여중인 모임글의 수를 반환한다
 }
