2c0657ebef9adf72ca70f84bf3bd1475242cffb9
yeon <choyeon960325@gmail.com>
2022-09-06 19:13:08 +0900

    [수정] QnA Board Controller 코드 수정

--- BackQnA/src/main/java/com/golflearn/controller/QnABoardController.java
+++ BackQnA/src/main/java/com/golflearn/controller/QnABoardController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.golflearn.dto.ResultBean;
 import com.golflearn.exception.AddException;
 import com.golflearn.exception.FindException;
 import com.golflearn.exception.ModifyException;
+import com.golflearn.exception.RemoveException;
 import com.golflearn.service.QnABoardService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -137,7 +139,7 @@ public class QnABoardController {
 	}
 
 	//글 상세보기
-	@GetMapping(value= {"view/{boardNo}"})
+	@GetMapping(value= "view/{boardNo}")
 	public ResultBean<QnABoardDto> viewBoard(@PathVariable Long boardNo, HttpSession session) {
 		ResultBean<QnABoardDto> rb = new ResultBean<>();
 		QnABoardDto qd = new QnABoardDto();
@@ -162,7 +164,7 @@ public class QnABoardController {
 	}
 
 	//글 작성하기
-	@PostMapping(value = {"write"})
+	@PostMapping(value = "write")
 	public ResultBean<QnABoardDto> writeboard(HttpSession session, @RequestBody QnABoardDto qnaBoard ) {
 		ResultBean<QnABoardDto> rb = new ResultBean<>();
 		String loginNickname = (String)session.getAttribute("loginNickname");
@@ -189,11 +191,11 @@ public class QnABoardController {
 	}
 
 
-	//	댓글(답변) 달기(관리자만 가능)
+	//댓글(답변) 달기(관리자만 가능)
 	@PostMapping(value= "comment", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResultBean<QnACommentDto> writeComment(HttpSession session, @RequestBody QnACommentDto qnaComment) {
 		ResultBean<QnACommentDto> rb = new ResultBean<>();
-		//			String userType = (String)session.getAttribute("userType");
+		//			int userType = session.getAttribute("userType");
 		//			String loginNickname = (String)session.getAttribute("loginNickname");
 
 		int userType = 2;
@@ -216,36 +218,82 @@ public class QnABoardController {
 		return rb;
 	}
 
-//	글 수정하기(댓글이 달렸으면 수정하지 못하게, 글 수정할 때 제목 본문 나옴)
-	@PutMapping(value = {"{boardNo}"})
+	//	글 수정하기(댓글이 달렸으면 수정하지 못하게, 글 수정할 때 제목 본문 나옴)
+	@PutMapping(value = "{boardNo}")
 	public ResultBean<QnABoardDto> modifyBoard(HttpSession session, @RequestBody QnABoardDto qnaBoard) {
 		ResultBean<QnABoardDto> rb = new ResultBean<>();
-//		String loginNickname = (String)session.getAttribute("loginNickname");	
+		//		String loginNickname = (String)session.getAttribute("loginNickname");	
 		String loginNickname = "데빌";
-			try {
-				if(!loginNickname.equals(qnaBoard.getUserNickname())||qnaBoard.getComment() != null) {
-					rb.setStatus(0);
-					rb.setMsg("요청에 실패했습니다");
-				}else {
-					Long boardNo = qnaBoard.getBoardNo();
-					qnaBoard.setBoardNo(boardNo);
-					qnaService.modifyBoard(qnaBoard);
-					rb.setStatus(1);
-					rb.setMsg("글이 정상적으로 수정되었습니다");
-				}
-			} catch (ModifyException e) {
-				e.printStackTrace();
+		Long boardNo = qnaBoard.getBoardNo();	
+		System.out.println(qnaBoard.getBoardNo());
+		try {	
+			if(!loginNickname.equals(qnaBoard.getUserNickname())) {
+				System.out.println(qnaBoard.getUserNickname());
+				rb.setStatus(0);
+				rb.setMsg("작성자만 수정이 가능합니다");
+
+			}else {
+				qnaBoard.setBoardNo(boardNo);
+				qnaService.modifyBoard(qnaBoard);
+				rb.setStatus(1);
+				rb.setMsg("글이 정상적으로 수정되었습니다");
 			}
+		} catch (ModifyException e) {
+			e.printStackTrace();
+		}
 		return rb;
 	}
 	//글 삭제하기(답변까지 삭제)
-
-
-
+	@DeleteMapping("{boardNo}")
+	public ResultBean<QnABoardDto> deleteBoard (HttpSession session, @PathVariable Long boardNo, @RequestBody QnABoardDto qnaBoard) throws RemoveException{
+		ResultBean<QnABoardDto> rb = new ResultBean<>();
+//		String loginNickname = (String)session.getAttribute("loginNickname");
+//		String userType = (String)session.getAttribute("userType");
+		//테스트용
+		String loginNickname = "데빌";
+		String userType = "2";
+//		boardNo = qnaBoard.getBoardNo();
+		try {
+//			qnaBoard.setBoardNo(boardNo);
+			qnaService.deleteBoard(boardNo);
+			if (!loginNickname.equals(qnaBoard.getUserNickname()) || !userType.equals("2") ) {
+				System.out.println(qnaBoard.getUserNickname());
+				rb.setStatus(0);
+				rb.setMsg("삭제는 작성자나 관리자만 가능합니다");
+			}else {
+				rb.setStatus(1);
+				rb.setMsg("글이 삭제 되었습니다");
+			}
+			return rb;
+		}catch (RemoveException e) {
+			e.printStackTrace();
+			return rb;
+		}
+	}
+	
 	//댓글 삭제하기(관리자만 가능)
-
-
-
-	//
-
+	@DeleteMapping("delete/{commentNo}")
+	public ResultBean<QnACommentDto> deleteComment(HttpSession session, @PathVariable Long commentNo, @RequestBody QnACommentDto qnaComment) {
+		ResultBean<QnACommentDto> rb = new ResultBean<>();
+//		String loginNickname = (String)session.getAttribute("loginNickname");
+//		String userType = (String)session.getAttribute("userType");
+		//테스트용
+		String loginNickname = "kosta";
+		String userType = "1";
+		try {
+			qnaService.deleteComment(commentNo);
+			if (loginNickname.equals(qnaComment.getUserNickname()) && userType.equals("2") ) {
+				rb.setStatus(1);
+				rb.setMsg("글이 삭제 되었습니다");
+				System.out.println(qnaComment.getUserNickname());
+			}else {
+				rb.setStatus(0);
+				rb.setMsg("삭제는 작성자나 관리자만 가능합니다");
+			}
+			return rb;
+		}catch(RemoveException e) {
+			e.printStackTrace();
+			return rb;
+		}
+	}
 }
