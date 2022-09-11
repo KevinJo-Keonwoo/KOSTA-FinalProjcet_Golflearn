2d77ffee5d7219ce643bd5965802e5fbb453bc56
future <rmh7290@gmail.com>
2022-09-11 17:13:07 +0900

    [수정] back 연결 후 정상 구동되도록 처리

--- front/src/main/webapp/jq/login.js
+++ front/src/main/webapp/jq/login.js
@@ -1,22 +1,26 @@
 $(function () {
 	//아이디 입력 객체 찾기
-	let $inputId = $("input[name=userId]");
+	let $inputId = $("input[name=user_id]");
 
 	//비밀번호 입력 객체 찾기
-	let $inputPwd = $("input[name=userPwd]");
+	let $inputPwd = $("input[name=user_pwd]");
 
 	// 데이터 한번에 전송 해 줄 form 객체 생성
 	let $form = $("form");
 	$form.submit(function () {
 		let url = "http://localhost:1124/back/user/login";
-		let inputIdValue = $inputId.val();
-		let inputPwdValue = $inputPwd.val();
-		let data = "userId=" + inputIdValue + "&userPwd=" + inputPwdValue;
-
+		// let inputIdValue = $inputId.val();
+		// let inputPwdValue = $inputPwd.val();
+		// let data = "userId=" + inputIdValue + "&userPwd=" + inputPwdValue;
+		console.log($inputId.val());
+		console.log($inputPwd.val());
     $.ajax({
 		url: url,
 		method: "post",
-		data: data,
+		data: {
+			userId : $inputId.val(),
+			userPwd : $inputPwd.val()
+		},
 		success: function (jsonObj) {
 			if (jsonObj.status == 1) {
 				alert(jsonObj.msg);
