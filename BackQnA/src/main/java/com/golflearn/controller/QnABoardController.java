2fc047c9f9f1e01e5d3a7f9eedc7fe8d28301b65
yeon <choyeon960325@gmail.com>
2022-09-12 11:01:21 +0900

    [수정] QnABoardController 코드 변경

--- BackQnA/src/main/java/com/golflearn/controller/QnABoardController.java
+++ BackQnA/src/main/java/com/golflearn/controller/QnABoardController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.golflearn.service.QnABoardService;
 
 import lombok.extern.slf4j.Slf4j;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("board/*")
 @Slf4j
@@ -41,7 +43,7 @@ public class QnABoardController {
 
 	//전체 리스트 불러오기
 	@GetMapping(value= {"list", "list/{optCp}"})
-	public ResultBean<PageBean<QnABoardDto>> list(@PathVariable Optional<Integer> optCp){
+	public ResultBean<PageBean<QnABoardDto>> list(@PathVariable("pageNo") Optional<Integer> optCp){
 		ResultBean<PageBean<QnABoardDto>> rb = new ResultBean<>();
 		try {
 			int currentPage;
